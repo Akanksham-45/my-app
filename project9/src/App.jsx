d909ef09f34cfac1cb7c5050cd3a9b77423c7a7e
Akanksha Maurya <akankshamaurya91627@gmail.com>
2026-03-18 00:38:21 +0530

    pro9

--- project9/src/App.jsx
+++ project9/src/App.jsx
@@ -4,7 +4,7 @@ import Student from "./Student";
 function App() {
   return (
     <div style={{textAlign:"center"}}>
-      <h1>Props Example</h1>
+      <h1>Props example</h1>
 
       <Student name="Rahul" age="20" />
       <Student name="Amit" age="22" />
