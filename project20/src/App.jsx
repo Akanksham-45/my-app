affdf630216d0298779ba9d030e99ed4efd6010b
Akanksha Maurya <akankshamaurya91627@gmail.com>
2026-04-09 11:34:52 +0530

    pro20

---
+++ project20/src/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.text}>CSS Module Example</h2>
+      
+    </div>
+  );
+}
+
+export default App;
