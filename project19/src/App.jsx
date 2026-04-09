95813efd5671534583ba9121adf3929ba526787c
Akanksha Maurya <akankshamaurya91627@gmail.com>
2026-04-09 11:34:26 +0530

    pro19

---
+++ project19/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="box">
+      <h2 className="heading">Hello React</h2>
+      <p>This is external CSS styling</p>
+    </div>
+  );
+}
+
+export default App;
