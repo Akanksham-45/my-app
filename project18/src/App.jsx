26b1f2a93c4c3f5aa721716d6c2c10b4cd5d67b1
Akanksha Maurya <akankshamaurya91627@gmail.com>
2026-04-09 11:33:55 +0530

    pro18

---
+++ project18/src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+  return (
+    <div style={{color: "white", backgroundColor: "blue", padding: "50px"}}>
+      <h1>Hello React</h1>
+      <p>This is Inline Styling</p>
+    </div>
+  );
+}
+
+export default App;
